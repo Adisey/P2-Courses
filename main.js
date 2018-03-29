71d3b1b5542d3e4eae1d3e75d6e22641290b42cf
Andrey Smirnov <37780231+Adisey@users.noreply.github.com>
2018-03-29 17:24:34 +0300

    Updete

---
+++ main.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+
+console.log(' Есть ')
+    var elems = document.documentElement.childNodes;
+    for (var key in elems) {
+        console.log( key ); // 0, 1, 2, length, item
+    }
+    //var par_1 = document.documentElement.g
+
+}
