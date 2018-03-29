a04ade6b3265be7fd6ecdc58eabd83c7dc567971
Andrey Smirnov <37780231+Adisey@users.noreply.github.com>
2018-03-30 01:42:29 +0300

    Updete

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-window.onload=function(){
+'use strict';
+window.onload = function () {
+    var elementFlash = ['h2_1', 'h2_2', 'h2_3']
+    onmouseover = function (event) {
+        var target = event.target;
+        if (elementFlash.indexOf(target.id) >= 0) {
+            target.style.background = 'cadetblue';
+        }
+    };
+    onmouseout = function (event) {
+        var target = event.target;
+        if (elementFlash.indexOf(target.id) >= 0) {
+            target.style.background = '';
+        }
+    };
+    onclick = function (event) {
+        var target = event.target;
+        if (elementFlash.indexOf(target.id) >= 0) {
+            var child_e = target.parentNode.childNodes;
+            for (var child_n in child_e){
+                if (child_e[child_n].localName == 'p'){
+                    child_e[child_n].hidden = ! child_e[child_n].hidden ;
+                }
+            }
 
-console.log(' Есть ')
-    var elems = document.documentElement.childNodes;
-    for (var key in elems) {
-        console.log( key ); // 0, 1, 2, length, item
-    }
-    //var par_1 = document.documentElement.g
-
+        }
+    };
 }
