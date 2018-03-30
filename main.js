9868a99ca224450f66ce5672720cddc3c4c8d37a
Andrey Smirnov <37780231+Adisey@users.noreply.github.com>
2018-03-31 00:23:02 +0300

    Updete

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 'use strict';
 window.onload = function () {
+    color_but.onclick = function () {
+        this.style.backgroundColor = 'red';
+    };
+    but_progr.onclick = function () {
+        this.style.backgroundColor = 'green';
+    };
+
+    plane_pic.onclick = function () {
+        var ended = false;
+        plane_pic.addEventListener('transitionend', function () {
+            if (!ended) {
+                ended = true;
+                alert('Есть !');
+            }
+        });
+        plane_pic.classList.add('growing');
+    };
+// ******************************
     var elementFlash = ['h2_1', 'h2_2', 'h2_3'];
     onmouseover = function (event) {
         var target = event.target;
@@ -21,18 +39,14 @@ window.onload = function () {
             for (var child_n in child_e) {
                 if (child_e[child_n].localName == 'div' && child_e[child_n].className == 'vewparag') {
                     var bb = child_e[child_n];
-                    child_e[child_n].hidden = !child_e[child_n].hidden;
+                    child_e[child_n].classList.add('collapsing');
+//                    child_e[child_n].hidden = !child_e[child_n].hidden;
                 }
             }
 
         }
     };
 
-// ******************************
-    color_but.onclick = function () {
-        this.style.backgroundColor = 'red';
-    };
-    but_progr.onclick = function () {
-        this.style.backgroundColor = 'green';
-    };
+
+// End
 }
