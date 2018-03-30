b9d67bc3fb05cbd19475f96edd5a1ced280b56d4
Andrey Smirnov <37780231+Adisey@users.noreply.github.com>
2018-03-30 15:49:42 +0300

    Updete

--- main.js
+++ main.js
@@ -18,11 +18,24 @@ window.onload = function () {
         if (elementFlash.indexOf(target.id) >= 0) {
             var child_e = target.parentNode.childNodes;
             for (var child_n in child_e){
-                if (child_e[child_n].localName == 'p'){
+                //console.log(child_e[child_n]);
+                if (child_e[child_n].localName == 'div' && child_e[child_n].className == 'vewparag'){
+//                    child_e[child_n].offsetHeight = child_e[child_n].offsetHeight /2;
                     child_e[child_n].hidden = ! child_e[child_n].hidden ;
+                    console.log('--1--');
+//                    child_e[child_n].classList.add('animate');
+
                 }
             }
 
         }
     };
+
+// ******************************
+    color.onclick = function() {
+        this.style.backgroundColor = 'red';
+    }
+
+
+
 }
